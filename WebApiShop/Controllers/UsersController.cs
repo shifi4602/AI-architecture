6d25fe394f1fd753ff645b8e2d2dc158124757c9
shifi4602 <39215601303@mby.co.il>
2025-11-30 22:03:39 +0200

    fix UsersController.cs

--- WebApiShop/Controllers/UsersController.cs
+++ WebApiShop/Controllers/UsersController.cs
@@ -10,89 +10,63 @@ namespace Enteties.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase, IUsersController
-    {
-        private readonly UsersService _usersService = new UsersService();
-
-        IUsersService _iUsersServicies;
+    {        
         IpasswordServices _iPasswordsServices;
-
+        
         public UsersController(IUsersService usersServicies, IpasswordServices passwordServices)
         {
             _iPasswordsServices = passwordServices;
-            _iUsersServicies = usersServicies;
+            _usersServicies = usersServicies;
         }
         
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return Ok("value");
         }
-
+        
         // POST api/<UsersController>
         [HttpPost]
         public ActionResult<Users> Post([FromBody] Users user)
         {
-            Users result = _usersService.AddNewUser(user);
+            Users result = _usersServicies.AddNewUser(user);
             if (result == null)
                 return BadRequest("Password is not strong enough");
             return CreatedAtAction(nameof(Get), new { result.Id }, result);
         }
-
+        
         [HttpPost("login")]
-        public ActionResult<Users> Login([FromBody] UpdateUser loginUser)
+        public ActionResult<Users> Login([FromBody] UpdateUser value)
         {
-            Users user = _usersService.Login(loginUser);
-
-        //List<users> user = new List<users>();
-
-        [HttpPost]
-        public ActionResult<Users> Post([FromBody] Users value)
-        {
-            Users user = _iUsersServicies.AddNewUser(value);
-            if (user == null)
-                return BadRequest("Password is too weak");
-            return CreatedAtAction(nameof(Get), new { user.id }, user);
-        }
-
-        [HttpPost("login")]
-        public ActionResult<Users> login([FromBody] UpdateUser value)
-        {
-            Users user = _iUsersServicies.Login(value);
+            Users user = _usersServicies.Login(value);
             if (user != null)
             {
                 return Ok(user);
             }
             return Unauthorized();
         }
-
+        
         // PUT api/<UsersController>/5
         [HttpPut("{id}")]
-        public ActionResult UpdateUser(int id, [FromBody] Users userToUpdate)
-
+        
         public IActionResult UpdateUser(int id, [FromBody] Users userToUpdate)
         {
-            bool passwordsStrenght = _iUsersServicies.UpdateUser(id, userToUpdate);
+            bool passwordsStrenght = _usersServicies.UpdateUser(id, userToUpdate);
             if (passwordsStrenght)
             {
                 return Ok(userToUpdate);
             }
             return NoContent();
-
+        
         }
-
+        
         // DELETE api/<UsersController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            _usersService.UpdateUser(id, userToUpdate);
-            return NoContent();
+            //_usersService.UpdateUser(id, userToUpdate);
+            //return NoContent();
         }
     }
 }
