e6c29dcb79ea44fed47c4eb238b370a72bee5764
shifi4602 <39215601303@mby.co.il>
2025-11-30 21:57:00 +0200

    fix userService

--- UsersServicies/UsersService.cs
+++ UsersServicies/UsersService.cs
@@ -1,50 +1,38 @@
-﻿
-using Enteties;
+﻿using Enteties;
 using Repositories;
 
 namespace Services
 {
     public class UsersService : IUsersService
     {
-
-        private readonly UsersRepository _usersRepository = new UsersRepository();
-        private readonly passwordServices _passwordServices = new passwordServices();
+        private readonly IUsersRepository _usersRepository = new UsersRepository();
+        private readonly IpasswordServices _passwordServices = new passwordServices();
         
+        public UsersService(IUsersRepository usersRepository, IpasswordServices passwordServices)
+        {
+            _usersRepository = usersRepository;
+            _passwordServices = passwordServices;
+        }
         public Users AddNewUser(Users user)
         {
-            if (_passwordServices.GetStrength(user.Password).Strength < 2)
+            if (_passwordServices.GetStrength(user.Password).Strength <= 2)
                 return null;
             return _usersRepository.AddUser(user);
-        IUsersRepository _iUsersRepository;
-        IpasswordServices _iPasswordServices;
-        public UsersService(IUsersRepository usersRepository, IpasswordServices passwordServices) 
-        {
-            _iUsersRepository = usersRepository;
-            _iPasswordServices = passwordServices;
         }
-        public Users AddNewUser(Users user)
-        {
-            if (_iPasswordServices.GetStrength(user.Password).Strength <= 2)
-                return null;
-            return _iUsersRepository.AddUser(user);
-        }
-
-        public Users Login(UpdateUser user)
         
+        public Users Login(UpdateUser user)
+        {
             return _usersRepository.Login(user);
-
-            return _iUsersRepository.login(user);
-
         }
-
+        
         public bool UpdateUser(int id, Users userToUpdate)
         {
             _usersRepository.UpdateUser(id, userToUpdate);
-            if (_iPasswordServices.GetStrength(userToUpdate.Password).Strength <= 2)
+            if (_passwordServices.GetStrength(userToUpdate.Password).Strength <= 2)
             {
                 return false;
             }
-            _iUsersRepository.UpdateUser(id, userToUpdate);
+            _usersRepository.UpdateUser(id, userToUpdate);
             return true;
         }
     }
