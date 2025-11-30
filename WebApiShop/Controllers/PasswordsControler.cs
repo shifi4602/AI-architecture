39dd7203b8d6729c45ae22ccf477871615ea62a0
shifi4602 <39215601303@mby.co.il>
2025-11-30 22:00:26 +0200

    fix PasswordsControler.cs

--- WebApiShop/Controllers/PasswordsControler.cs
+++ WebApiShop/Controllers/PasswordsControler.cs
@@ -11,50 +11,27 @@ namespace Enteties.Controllers
 
     public class PasswordsController : ControllerBase
     {
-        private readonly passwordServices _passwordService = new passwordServices();
-
-        // POST api/<PasswordsController>
-        [HttpPost("{pass}")]
-        public ActionResult<PassEntity> CheckPasswordStrength([FromBody] string pass)
-        {
-            PassEntity password = _passwordService.GetStrength(pass);
-
-    public class PasswordsController : ControllerBase, IPasswordsController
-    {
         IpasswordServices _iPasswordService;
         public PasswordsController(IpasswordServices iPasswordController)
         {
             _iPasswordService = iPasswordController;
         }
-        // GET: api/<PasswordController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<PasswordController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
+   
         [HttpGet]
         public void Get(string pass)
         {
-
+        
         }
-
+        
         // POST api/<PasswordController>
         [HttpPost("CheckPasswordStrength")]
         public ActionResult<PassEntity> CheckPasswordStrength([FromBody] string pass)
         {
             PassEntity password = _iPasswordService.GetStrength(pass);
-
+        
             if (password == null)
                 return BadRequest("Invalid password");
             return Ok(password);
         }
-    }
+    }        
 }
